d5b11cc8b2659dbd324cea444a9e55d59388f07c
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-09-07 16:41:38 -0300

    Código inicial do projeto

---
+++ Lab05/CodigoCk/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Isso é para exibir uma menssagem com meu nome
+        System.out.println("Meu nome: Marco");
+        // Isso é para exibir o nome da dsiciplinajava -jar ck-x.x.x-SNAPSHOT-jar-with-dependencies.jar
+        System.out.println("Nome da disciplina: Engenharia Econômica para Software");
+    }
+}
